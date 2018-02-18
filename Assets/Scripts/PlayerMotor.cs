03d497fb8ef19f42c2255051ee22ebbafec7c0fd
JonasKoenig <jonas-koenig@gmx.net>
2018-02-18 12:59:47 +0100

    Movement less weird

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -7,8 +7,11 @@ public class PlayerMotor : MonoBehaviour {
     public string moveRight;
     public string moveLeft;
 	
-	private float speed = 0.2f;
-    private float jumpSpeed = 3f;
+	private float midAirSpeed = 0.2f;
+    private float groundSpeed = 10f;
+    private float jumpVelocity = 12f;
+    private float fallMultiplier = 2f;
+    private float lowJumpMultiplier = 2.5f;
 
     private KeyCode up;
 	private KeyCode left;
@@ -23,20 +26,62 @@ public class PlayerMotor : MonoBehaviour {
         rb = GetComponent<Rigidbody>();
     }
 
-	void Update() {
-        Boolean grounded = Physics.Raycast(transform.position, Vector3.down, 0.1f);
-        if (Input.GetKey(up) && grounded)
+	void FixedUpdate() {
+
+        // movement according to user input
+        InputToMovement();
+
+        // snappy jumping/falling
+        if (rb.velocity.y < 0)
         {
-            Vector3 direction = new Vector3(0f, jumpSpeed, 0f);
-            rb.AddForce(direction, ForceMode.Impulse);
-        } else if (Input.GetKey(left))
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        } else if (rb.velocity.y > 0 && !Input.GetKey(up))
         {
-            Vector3 direction = new Vector3(speed, 0f, 0f);
-            rb.AddForce(direction, ForceMode.VelocityChange);
-        }
-        else if (Input.GetKey(right)) {
-            Vector3 direction = new Vector3(-speed, 0f, 0f);
-            rb.AddForce(direction, ForceMode.VelocityChange);
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
     }
+
+    void InputToMovement()
+    {
+        bool upInput = Input.GetKey(up);
+        bool leftInput = Input.GetKey(left);
+        bool rightInput = Input.GetKey(right);
+
+        Vector3 upMovement = Vector3.zero;
+        Vector3 leftMovement = Vector3.zero;
+        Vector3 rightMovement = Vector3.zero;
+
+        Boolean grounded = Physics.Raycast(transform.position, Vector3.down, 0.1f);
+        if (grounded)
+        {
+            rb.velocity = new Vector3(0f, rb.velocity.y, 0f);
+        }
+        if (upInput && grounded && rb.velocity.y <= 0)
+        {
+            upMovement = Vector3.up * jumpVelocity;
+        }
+        if (leftInput && !rightInput)
+        {
+            if (grounded)
+            {
+                rb.velocity = new Vector3(groundSpeed, rb.velocity.y, 0f);
+            } else
+            {
+                leftMovement = Vector3.right * midAirSpeed;
+            }
+        }
+        if (rightInput && !leftInput)
+        {
+            if (grounded)
+            {
+                rb.velocity = new Vector3(-groundSpeed, rb.velocity.y, 0f);
+            } else
+            {
+            leftMovement = Vector3.left * midAirSpeed;
+            }
+        }
+
+        rb.AddForce(upMovement + leftMovement + rightMovement, ForceMode.Impulse);
+    }
+
 }
